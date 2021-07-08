4484722871b7581040cac4903dd36b3a01df1e31
Zartab Nakhwa <nzartab@gmail.com>
2021-07-08 19:28:11 +0530

    Flex box implementation

--- app/components/FlexBoxComponent.js
+++ app/components/FlexBoxComponent.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export  function FlexBoxComponent() {
+export function FlexBoxComponent() {
     return (
         <View style={{
-            backgroundColor:'dodgerblue',
-            flex:0.5,
-         }}>
-          
+            backgroundColor: '#eee',
+            flex: 1,
+        }}>
+
+            <View style={{
+                backgroundColor: 'dodgerblue',
+                flex: 2,
+            }} />
+            <View style={{
+                backgroundColor: 'gold',
+                flex: 1,
+            }} />
+            <View style={{
+                backgroundColor: 'tomato',
+                flex: 1,
+            }} />
+
+
+
         </View>
     )
 }
