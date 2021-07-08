76dee020d8b0dbf0a4caafbd8a5f44764ecd6791
Zartab Nakhwa <nzartab@gmail.com>
2021-07-08 18:12:27 +0530

    Using Dimensions

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Dimensions} from 'react-native';
 
 export default function App() {
+
+  console.log(Dimensions.get('screen'))
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{
+        backgroundColor:'dodgerblue',
+        width:'50%',
+        height:70
+      }}
+      >
+      </View>
     </View>
   );
 }
@@ -15,7 +23,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    marginTop:50,
   },
 });
