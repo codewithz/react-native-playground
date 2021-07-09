b4cca0b4eee949d32a64e352a25f1be405f8c3f1
Zartab Nakhwa <nzartab@gmail.com>
2021-07-09 19:15:08 +0530

    Text Styling

--- app/components/styling/TextStyleComponent.js
+++ app/components/styling/TextStyleComponent.js
@@ -4,7 +4,11 @@ import {Text,View,StyleSheet} from 'react-native';
 export function TextStyleComponent(props) {
     return (
         <View style={styles.container}>
-        <Text>Hello</Text>
+        <Text style={styles.textContent}>
+            I love React Native!!
+            My first React native app!!
+            Here is some more text for you guys
+            </Text>
         </View>
     );
 }
@@ -13,7 +17,18 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        padding:20,
+    },
+    textContent:{
+        fontFamily:'Roboto',
+        fontSize:30,
+        fontStyle:'italic',
+        fontWeight:'600',
+        color:'tomato',
+        textTransform:'capitalize',
+        textAlign:'center',
+        lineHeight:40
     }
 })
 
